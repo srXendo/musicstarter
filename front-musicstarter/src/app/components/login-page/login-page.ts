ac4ca872b044592613795469d5a172508ad96b4c
srXendo <david.martin.melgar@gmail.com>
2025-08-19 18:22:20 +0200

    add: login

--- front-musicstarter/src/app/components/login-page/login-page.ts
+++ front-musicstarter/src/app/components/login-page/login-page.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css'
 })
 export class LoginPage {
+  error_login_temporal_user: boolean = false;
+  error_login_user: boolean = false;
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
   constructor(private loginSerivice: LoginService){}
   login_temporal_user(){
     this.loginSerivice.login_temporal_user().subscribe({
@@ -17,7 +24,11 @@ export class LoginPage {
       error: (err: any)=>{
         console.error(err)
         console.error(new Error(err.stack))
+        this.error_login_temporal_user = true
       }
     })
+  }
+  login_user(){
+    this.error_login_user = true
   }
 }
